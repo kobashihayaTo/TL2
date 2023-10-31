33b01391237e4b1ffff8405d98b55a9e019c0990
kobashi_hayato <k021g1128@g.neec.ac.jp>
2023-10-31 14:44:43 +0900

    完成

--- TextureConverter.h
+++ TextureConverter.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <DirectXTex.h>
 #include <string>
+
 class TextureConverter
 {
 public:
@@ -12,16 +13,41 @@ public:
 
 private:
 	/// <summary>
+	/// DDSテクスチャとしてファイル書き出し
+	/// </summary>
+	void SaveDDSTextureToFile();
+
+	/// <summary>
 	///テクスチャ読み込み
 	/// </summary>
 	/// <param name="filePath">ファイルパス<param>
 	void LoadWICTextureFromFile(const std::string& filePath);
-	
+
 	/// <summary>
 	/// マルチバイト文字列をワイド文字列に変換
 	/// </summary>
 	/// <param name="filePath">マルチバイト文字列<param>
 	///<returns>ワイド文字列</returns>
 	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
+
+	/// <summary>
+	/// フォルダパスとファイル名を分離する
+	/// </summary>	
+	/// <param name="filePath">ファイルパス<param>
+	void SeparateFilePath(const std::wstring& filePath);
+
+private:
+	//画像の情報
+	DirectX::TexMetadata metadata_;
+	//画像イメージのコンテナ
+	DirectX::ScratchImage scratchImage_;
+
+
+	//ディレクトリパス
+	std::wstring directoryPath_;
+	//ファイル名
+	std::wstring fileName_;
+	//ファイル拡張子
+	std::wstring fileExt_;
 };
 
